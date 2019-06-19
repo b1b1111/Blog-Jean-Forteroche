30c16a1a24ca9af5b659ebfbdbf76c04d85fe468
b1b1111 <benjamin@webagency-lefebvre.fr>
2019-06-19 16:25:36 +0200

    change sql

--- view/frontend/administration_post.php
+++ view/frontend/administration_post.php
@@ -3,7 +3,7 @@
     $title = $_POST['title'];
     $content = $_POST['content'];
     
-    $bdd = new mysqli('localhost','root','','Jean Forteroche');
+    $bdd = new mysqli('localhost','webagencawben','Ben0ubenou','webagencawben');
 
     if ($bdd->connect_error) {
         echo 'database connect error';
